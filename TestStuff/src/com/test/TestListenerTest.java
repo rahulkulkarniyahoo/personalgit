4887f269b5595af1e1a06a593584c5a6f9b0b871
Rahul Kulkarni <Rahul.Kulkarni@mailnewspapers.co.uk>
2015-08-19 16:56:01 +0100

    Added Listener Code.. not sure if this works

---
+++ TestStuff/src/com/test/TestListenerTest.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestListenerTest {
+
+  @Test
+  public void takeScreenShot() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
